d1b2f59338a4d10669b9596b5e2c8309d47ae431
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-09-10 18:18:08 +0200

    Change scripts

---
+++ Pong/Assets/Scripts/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car{
+    private string model;
+    private string brand;
+    private string plate;
+
+    public Car(string model, string brand, string plate){
+        this.model = model;
+        this.brand = brand;
+        this.plate = plate;
+    }
+
+    public string getModel(){
+        return model;
+    }
+
+    public string getBrand(){
+        return brand;
+    }
+
+    public void setPlate(string plate){
+        this.plate = plate;
+    }
+
+    public string getPlate(){
+        return plate;
+    }
+
+}
--- Pong/Assets/Scripts/Helloworld.cs
+++ Pong/Assets/Scripts/Helloworld.cs
@@ -4,39 +4,24 @@ using UnityEngine;
 
 public class Helloworld : MonoBehaviour {
 
-    private int somma;
-    private string message;
-
-    string[] Stati = { "Francia", "Italia", "Germania", "Inghilterra" };
+    public bool choise = false;
 
     private void Start()
     {
-        for (int i = 0; i <= 10; i++){
-            print(somma);
-            somma += 1;
+        if(choise){
+            print("The choise is true");
+        }else{
+            print("The choise is false");
         }
+        Car firstCar = new Car("Opel","Corsa","XXXXXXX");
+        Car superCar = new Car("Batmobile", "Deluxe", "BATMAN90");
 
-        message = "Salve Mondo";
-        print(message);
+        print(firstCar.getBrand());
+        print(firstCar.getModel());
+        print(firstCar.getPlate());
 
-        for (int i = 0; i < Stati.Length; i++) { 
-            print(Stati[i]); 
-        }
-
-        int x = 5;
-
-        x++;
-
-        Resetta(x);
-
-        x++;
-
-        print(x);
-
-    }
-
-    public void Resetta( int i ){
-        i = 0;
-        print(i);
+        print(superCar.getBrand());
+        print(superCar.getModel());
+        print(superCar.getPlate());
     }
 }
